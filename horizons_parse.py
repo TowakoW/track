5d48947f74b54f02777b943adf9addd4622ea561
Towako Wunsch <towakowunsch@berkeley.edu>
2026-08-23 22:55:23 -0700

    Starting moon implementation

--- horizons_parse.py
+++ horizons_parse.py
@@ -5,19 +5,17 @@ from astropy import units as u
 from typing import Literal, Any
 
 
-def horizons_specifics(naifids: list[int], data_type: Literal["gm", "ephemeris", "all"]) -> Any:
+def horizons_specifics(naifids: list[int]) -> Any:
     """
-    Organizes fetched data from NASA JPL's Horizons API
+    Organizes fetched data from NASA JPL's Horizons API, returns GM and ephemeris
 
     Parameters:
     -----
     "naifids": a list of one or more NAIF-IDs to object centers.
-    "data_type": specify either "gm" or "ephemeris" to pull data for specified objects(s).
-        > "gm": returns and prints GM for object.
-        > "ephemeris": returns ephemeris for object at time called (x, y, z, vx, vy, vz).
-        > "all": prints entire output from Horizons API (for debugging purposes...).
 
     All values returned in km/s
+
+    Note: this function has undergone a LOT of different versions, ignore all commented lines please...
     """        
     # Allow a single int NAIF-ID or an iterable of NAIF-IDs
     if isinstance(naifids, int):
@@ -25,29 +23,33 @@ def horizons_specifics(naifids: list[int], data_type: Literal["gm", "ephemeris",
 
     # Pulls ephimeris data from NASA JPL's Horizons API
     planet_data = []
-    for cmd in naifids:
-        planet_data.append({"naifid": cmd, "data": fetch_horizons_data(cmd)})
+    for naifid in naifids:
+        # planet_data.append({"naifid": naifid, "data": fetch_horizons_data(naifid)})
+        response = fetch_horizons_data(naifid)
 
-    if data_type == "gm":
+    # if data_type == "gm":
         # Parse GM data for each id in NAIF-ID
-        for item in planet_data:
-            raw = item["data"]["result"]
-            lines = raw.splitlines()
+    # for item in planet_data:
+        raw = response["result"]
+        lines = raw.splitlines()
 
-            gm_line = next(
+        gm_line = next(
                 (line for line in lines if "GM, km^3/s^2" in line or "GM (km^3/s^2)" in line or "GM (planet) km^3/s^2" in line),
                 None,
             )
-            gm_value = None
-            if gm_line:
+        gm = None
+        if gm_line is not None:
                 parts = gm_line.split("=")
-                if len(parts) > 1:
-                    gm_value = parts[1].split()[0].strip()
 
-            gm_val_km = None
-            if gm_value is not None:
-                gm_val_km = float(gm_value) * (u.km**3 / u.s**2)
-                item["gm"] = gm_val_km
+                if len(parts) > 1:
+                    gm_text = parts[1].split()[0]
+                    gm_value = float(gm_text.split("+=")[0])
+                    gm = gm_value * (u.km**3/u.s**2)
+
+        # gm_val_km = None
+        # if gm_value is not None:
+        #         gm_val_km = float(gm_value) * (u.km**3 / u.s**2)
+        #         item["gm"] = gm_val_km
 
             # # gm to (AU^3/s^2)
             # if gm_val_km is not None:
@@ -56,40 +58,55 @@ def horizons_specifics(naifids: list[int], data_type: Literal["gm", "ephemeris",
             # else:
             #     item["gm"] = None
 
-            print("NAIF-ID:", item["naifid"])
-            print("GM:", item["gm"] if gm_line is not None else "not found")
-            return item.get("gm")
+        # print("NAIF-ID:", item["naifid"])
+        # print("GM:", item["gm"] if gm_line is not None else "not found")
+            # return item.get("gm")
         
 
 
     # Parse ephemeris data for each dictionary in NAIF-ID
-    if data_type == "ephemeris":
-        for item in planet_data:
-            raw = item["data"]["result"]
-            lines = raw.splitlines()
+    # if data_type == "ephemeris":
+    # for item in planet_data:
+            # raw = item["data"]["result"]
+            # lines = raw.splitlines()
 
-            start = lines.index("$$SOE") + 1
-            end = lines.index("$$EOE")
-            table_lines = [line for line in lines[start:end] if line.strip()]
-            rows = [line.split(",") for line in table_lines]
+        start = lines.index("$$SOE") + 1
+        end = lines.index("$$EOE")
+
+        table_lines = [line for line in lines[start:end] if line.strip()]
+        rows = [[value.strip() for value in line.split(",")]
+        for line in table_lines]
 
             # Ephemeris data saved as a list udner "rows"
-            print("NAIF-ID:", item["naifid"])
+            # print("NAIF-ID:", item["naifid"])
             # print("--- EPHEMERIS LINES ---")
-            for num in rows:
+            # for num in rows:
                 # print(num)
-                return(num)
+                # return(num)
 
-    if data_type == "all":
-        for item in planet_data:
-            raw = item["data"]["result"]
-            lines = raw.splitlines()
-            rows = [line.split(",") for line in lines]
+    # if data_type == "all":
+        # for item in planet_data:
+        #     raw = item["data"]["result"]
+        #     lines = raw.splitlines()
+        #     rows = [line.split(",") for line in lines]
 
-            print("NAIF-ID:", item["naifid"])
-            for num in rows:
-                print(num)
+        #     print("NAIF-ID:", item["naifid"])
+        #     for num in rows:
+        #         print(num)
+        
+    # print("NAIF-ID:", item["naifid"])
+    planet_data.append(
+         {
+              "naifid": naifid,
+              "gm": gm,
+              "ephemeris": rows,
+         }
+    )
 
+    return planet_data
+    
+
+    
 
     # ALT: printing all output, comment out above for loop
     # print(lines)
--- planet_data.py
+++ planet_data.py
@@ -77,30 +77,61 @@ def get_initial_conditions(initial_condition: dict
         """
 
         # GM values (AU^3/s^2)
+        sun_data = horizons_specifics(10),
+        mercury_data = horizons_specifics(199),
+        venus_data = horizons_specifics(299),
+        earth_data = horizons_specifics(399),
+        mars_data = horizons_specifics(499),
+        jupiter_data = horizons_specifics(599),
+        saturn_data = horizons_specifics(699),
+        uranus_data = horizons_specifics(799),
+        neptune_data = horizons_specifics(899),
+        pluto_data = horizons_specifics(999)
+        moon_data = horizons_specifics(301),
+        phobos_data = horizons_specifics(401),
+        deimos_data = horizons_specifics(402),
+        io_data = horizons_specifics(501),
+        europa_data = horizons_specifics(502),
+        ganymede_data = horizons_specifics(503)
+
+
+        # GM data:
         GM_AU_S = {
-                "Sun": horizons_specifics(10, 'gm'),
-                "Mercury": horizons_specifics(199, 'gm'),
-                "Venus": horizons_specifics(299, 'gm'),
-                "Earth": horizons_specifics(399, 'gm'),
-                "Mars": horizons_specifics(499, 'gm'),
-                "Jupiter": horizons_specifics(599, 'gm'),
-                "Saturn": horizons_specifics(699, 'gm'),
-                "Uranus": horizons_specifics(799, 'gm'),
-                "Neptune": horizons_specifics(899, 'gm'),
-                "Pluto": horizons_specifics(999, 'gm')
+                "Sun": sun_data['gm'],
+                "Mercury": mercury_data['gm'],
+                "Venus": venus_data['gm'],
+                "Earth": earth_data['gm'],
+                "Mars": mars_data['gm'],
+                "Jupiter": jupiter_data['gm'],
+                "Saturn": saturn_data['gm'],
+                "Uranus": uranus_data['gm'],
+                "Neptune": neptune_data['gm'],
+                "Pluto": pluto_data['gm'],
+                "Moon": moon_data['gm'],
+                "Phobos": phobos_data['gm'],
+                "Deimos": deimos_data['gm'],
+                'Io': io_data['gm'],
+                "Europa": europa_data['gm'],
+                "Ganymede": ganymede_data['gm']
         }
 
         # Ephemeris data:
-        sun_eph = horizons_specifics(10, 'ephemeris')
-        mercery_eph = horizons_specifics(199, 'ephemeris')
-        venus_eph = horizons_specifics(299, 'ephemeris')
-        earth_eph = horizons_specifics(399, 'ephemeris')
-        mars_eph = horizons_specifics(499, 'ephemeris')
-        jupiter_eph = horizons_specifics(599, 'ephemeris')
-        saturn_eph = horizons_specifics(699, 'ephemeris')
-        uranus_eph =  horizons_specifics(799, 'ephemeris')
-        neptune_eph = horizons_specifics(899, 'ephemeris')
-        pluto_eph = horizons_specifics(999, 'ephemeris')
+        sun_eph = sun_data['ephemeris']
+        mercery_eph = mercury_data['ephemeris']
+        venus_eph = venus_data['ephemeris']
+        earth_eph = earth_data['ephemeris']
+        mars_eph = mars_data['ephemeris']
+        jupiter_eph = jupiter_data['ephemeris']
+        saturn_eph = saturn_data['ephemeris']
+        uranus_eph =  uranus_data['gm']
+        neptune_eph = neptune_data['ephemeris']
+        pluto_eph = pluto_data['ephemeris']
+        moon_eph = moon_data['ephemeris']
+        phobos_eph = phobos_data['ephemeris']
+        deimos_eph = deimos_data['ephemeris']
+        io_eph = io_data['ephemeris']
+        europa_eph = europa_data['ephemeris']
+        ganymede_eph = ganymede_data['ephemeris']
 
 
         # ephemeris rows: [jd, datetime, x, y, z, vx, vy, vz, ...]
@@ -114,7 +145,13 @@ def get_initial_conditions(initial_condition: dict
             "Saturn": saturn_eph[2:5],
             "Uranus": uranus_eph[2:5],
             "Neptune": neptune_eph[2:5],
-            "Pluto": pluto_eph[2:5]
+            "Pluto": pluto_eph[2:5],
+            "Moon": moon_eph[2:5],
+            "Phobos": phobos_eph[2:5],
+            "Deimos": deimos_eph[2:5],
+            "Io": io_eph[2:5],
+            "Europa": europa_eph[2:5],
+            "Ganymede": ganymede_eph[2:5]
         }
 
         SOLAR_SYSTEM_VEL = {
@@ -127,7 +164,13 @@ def get_initial_conditions(initial_condition: dict
             "Saturn": saturn_eph[5:8],
             "Uranus": uranus_eph[5:8],
             "Neptune": neptune_eph[5:8],
-            "Pluto": pluto_eph[5:8]
+            "Pluto": pluto_eph[5:8],
+            "Moon": moon_eph[5:8],
+            "Phobos": phobos_eph[5:8],
+            "Deimos": deimos_eph[5:8],
+            "Io": io_eph[5:8],
+            "Europa": europa_eph[5:8],
+            "Ganymede": ganymede_eph[5:8]
             }
 
         SOLAR_SYSTEM_COLORS = {
@@ -140,7 +183,15 @@ def get_initial_conditions(initial_condition: dict
                 "Saturn": 'slategrey',
                 "Uranus": 'olive',
                 "Neptune": 'teal',
-                "Pluto": 'aquamarine'
+                "Pluto": 'aquamarine',
+                "Moon": 'grey',
+                "Phobos": 'tan',
+                "Deimos": 'cornflowerblue',
+                "Io": 'lawngreen',
+                "Europa": 'pink',
+                "Ganymede": 'brown'
+
+
 
         }
 
@@ -170,6 +221,12 @@ def get_initial_conditions(initial_condition: dict
             G8 = np.array(GM_AU_S["Uranus"])
             G9 = np.array(GM_AU_S["Neptune"])
             G10 = np.array(GM_AU_S["Pluto"])
+            G11 = np.array(GM_AU_S["Moon"])
+            G12 = np.array(GM_AU_S["Phobos"])
+            G13 = np.array(GM_AU_S["Deimos"])
+            G14 = np.array(GM_AU_S['Io'])
+            G15 = np.array(GM_AU_S["Europa"])
+            G16 = np.array(GM_AU_S['Ganymede'])
             
             R1 = np.array(SOLAR_SYSTEM_POS["Sun"], dtype=float)
             R2 = np.array(SOLAR_SYSTEM_POS["Mercury"], dtype=float)
@@ -181,6 +238,14 @@ def get_initial_conditions(initial_condition: dict
             R8 = np.array(SOLAR_SYSTEM_POS["Uranus"], dtype=float)
             R9 = np.array(SOLAR_SYSTEM_POS["Neptune"], dtype=float)
             R10 = np.array(SOLAR_SYSTEM_POS["Pluto"], dtype=float)
+            R11 = np.array(SOLAR_SYSTEM_POS["Moon"], dtype=float)
+            R12 = np.array(SOLAR_SYSTEM_POS["Phobos"], dtype=float)
+            R13 = np.array(SOLAR_SYSTEM_POS["Deimos"], dtype=float)
+            R14 = np.array(SOLAR_SYSTEM_POS["Io"], dtype=float)
+            R15 = np.array(SOLAR_SYSTEM_POS["Europa"], dtype=float)
+            R16 = np.array(SOLAR_SYSTEM_POS["Ganymede"], dtype=float)
+
+
 
             V1 = np.array(SOLAR_SYSTEM_VEL["Sun"], dtype=float)
             V2 = np.array(SOLAR_SYSTEM_VEL["Mercury"], dtype=float)
@@ -192,6 +257,13 @@ def get_initial_conditions(initial_condition: dict
             V8 = np.array(SOLAR_SYSTEM_VEL["Uranus"], dtype=float)
             V9 = np.array(SOLAR_SYSTEM_VEL["Neptune"], dtype=float)
             V10 = np.array(SOLAR_SYSTEM_VEL["Pluto"], dtype=float)
+            V11 = np.array(SOLAR_SYSTEM_VEL["Moon"], dtype=float)
+            V12 = np.array(SOLAR_SYSTEM_VEL["Phobos"], dtype=float)
+            V13 = np.array(SOLAR_SYSTEM_VEL["Deimos"], dtype=float)
+            V14 = np.array(SOLAR_SYSTEM_VEL["Io"], dtype=float)
+            V15 = np.array(SOLAR_SYSTEM_VEL["Europa"], dtype=float)
+            V16 = np.array(SOLAR_SYSTEM_VEL["Ganymede"], dtype=float)
+
 
             Gm = np.array(
                    [
@@ -204,7 +276,13 @@ def get_initial_conditions(initial_condition: dict
                          G7,
                          G8,
                          G9,
-                         G10 
+                         G10,
+                         G11,
+                         G12,
+                         G13,
+                         G14,
+                         G15,
+                         G16
                    ]
             )
 
@@ -219,7 +297,13 @@ def get_initial_conditions(initial_condition: dict
                         R7,
                         R8,
                         R9,
-                        R10
+                        R10,
+                        R11,
+                        R12,
+                        R13,
+                        R14,
+                        R15,
+                        R16
                     ]
                 )
             v = np.array(
@@ -233,12 +317,18 @@ def get_initial_conditions(initial_condition: dict
                         V7,
                         V8,
                         V9,
-                        V10
+                        V10,
+                        V11,
+                        V12,
+                        V13,
+                        V14,
+                        V15,
+                        V16
                     ]
                 )
 
             system = System(
-                num_particles= 10,
+                num_particles= 16,
                 Gm=Gm,
                 x=x,
                 v=v,
